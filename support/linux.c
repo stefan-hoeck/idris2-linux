2aa38a7a7043c21a0d5349d84f2408a53b472640
stefan-hoeck <hock@zhaw.ch>
2024-08-25 09:44:17 +0200

    [ lint ] fix some more linting issues

--- support/linux.c
+++ support/linux.c
@@ -1,9 +1,7 @@
 // Copyright 2024 Stefan Höck
 
 #include <errno.h>
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
-uint32_t li_errno() {
-    return errno;
-}
+uint32_t li_errno() { return errno; }
